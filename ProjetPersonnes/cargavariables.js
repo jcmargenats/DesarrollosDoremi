9f77bcee49a79a26a6e0249eec9277fa41d6faeb
Juan Carlos Margenats <jcmargenats@gmail.com>
2020-05-28 09:45:45 -0400

    confir

---
+++ ProjetPersonnes/cargavariables.js
@@ -0,0 +1,16 @@
+u_nom1 = document.getElementById("nom1").value;
+u_nom2 = document.getElementById("nom2").value;
+u_nomfam = document.getElementById("nomfam").value;
+u_init = document.getElementById("init").value;
+u_traitem = document.getElementById("traitem").value;
+u_nomemp = document.getElementById("nomemp").value;
+u_numciv = document.getElementById("numciv").value;
+u_rue = document.getElementById("rue").value;
+u_ville = document.getElementById("ville").value;
+u_prov = document.getElementById("prov").value;
+u_codpos = document.getElementById("codpos").value;
+u_telephone = document.getElementById("telephone").value;
+u_courriel = document.getElementById("courriel").value;
+u_docident = document.getElementById("docident").value;
+u_nom1 = document.getElementById("genre").value;
+console.log(`2 El valor de rue es, ${u_rue}`);
