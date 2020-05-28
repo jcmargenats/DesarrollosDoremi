af5d706c8a0b6365689a445d2dab620246029660
Juan Carlos Margenats <jcmargenats@gmail.com>
2020-05-28 06:18:53 -0400

    Confirmacios

---
+++ ProjetPersonnes/coordonnees.js
@@ -0,0 +1,11 @@
+//  Function Coordonnees
+function Coordonnees(numciv, rue, ville, prov, codpos, telephone, courriel) {
+  (this.numciv = numciv),
+    (this.rue = rue),
+    (this.ville = ville),
+    (this.prov = prov),
+    (this.codpos = codpos),
+    (this.telephone = telephone),
+    (this.courriel = courriel);
+}
+//  Functions pour l'objet coordonnees
